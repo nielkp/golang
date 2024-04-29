3fc582914d8b28e505dceaa2fbebedd8a331f7bf
Daniel Knaip <77499075+nielkp@users.noreply.github.com>
2024-04-29 01:11:39 -0300

    Update desafio2.go

--- desafio2.go
+++ desafio2.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("Pin Pam")
+			fmt.Println("Pin Pan")
 		} else if i%3 == 0 {
 			fmt.Println("Pin")
 		} else if i%5 == 0 {
-			fmt.Println("Pam")
+			fmt.Println("Pan")
 		} else {
 			fmt.Println(i)
 		}
